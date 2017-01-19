e05e6da172e5377250f4f36db2ef83c00fdb243a
geethuj <geethuj@qburst.com>
2017-01-19 14:29:00 +0530

    Removing files

--- threex.planets.js
+++ threex.planets.js
@@ -1,90 +1,95 @@
 var THREEx = THREEx || {}
 
-THREEx.Planets	= {}
+THREEx.Planets = {}
 
-THREEx.Planets.baseURL	= '../'
+THREEx.Planets.baseURL = '../'
 
-THREEx.Planets.createEarth	= function(){
-	var geometry	= new THREE.SphereGeometry(1, 32, 32)
-	var material	= new THREE.MeshPhongMaterial({
-		map		: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/earthmap1k.jpg'),
-		bumpMap		: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/earthbump1k.jpg'),
-		bumpScale	: 0.05,
-		specularMap	: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/earthspec1k.jpg'),
-		specular	: new THREE.Color('grey'),
-	})
-	var mesh	= new THREE.Mesh(geometry, material)
-	return mesh
+THREEx.Planets.createEarth = function() {
+    var geometry, material, mesh;
+    geometry = new THREE.SphereGeometry(1, 32, 32)
+    material = new THREE.MeshPhongMaterial({
+        map: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL + 'images/earthmap1k.jpg'),
+        bumpMap: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL + 'images/earthbump1k.jpg'),
+        bumpScale: 0.05,
+        specularMap: THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL + 'images/earthspec1k.jpg'),
+        specular: new THREE.Color('grey'),
+    })
+    mesh = new THREE.Mesh(geometry, material)
+    return mesh
 }
 
-THREEx.Planets.createEarthCloud	= function(){
-	// create destination canvas
-	var canvasResult	= document.createElement('canvas')
-	canvasResult.width	= 1024
-	canvasResult.height	= 512
-	var contextResult	= canvasResult.getContext('2d')
+THREEx.Planets.createEarthCloud = function() {
+    var canvasResult, contextResult, imageMap, geometry, material, mesh;
 
-	// load earthcloudmap
-	var imageMap	= new Image();
-	imageMap.addEventListener("load", function() {
+    // create destination canvas
+    canvasResult = document.createElement('canvas')
+    canvasResult.width = 1024
+    canvasResult.height = 512
+    contextResult = canvasResult.getContext('2d')
 
-		// create dataMap ImageData for earthcloudmap
-		var canvasMap	= document.createElement('canvas')
-		canvasMap.width	= imageMap.width
-		canvasMap.height= imageMap.height
-		var contextMap	= canvasMap.getContext('2d')
-		contextMap.drawImage(imageMap, 0, 0)
-		var dataMap	= contextMap.getImageData(0, 0, canvasMap.width, canvasMap.height)
+    // load earthcloudmap
+    imageMap = new Image();
+    imageMap.addEventListener("load", function() {
+        var canvasMap, contextMap, dataMap, imageTrans;
+        // create dataMap ImageData for earthcloudmap
+        canvasMap = document.createElement('canvas')
+        canvasMap.width = imageMap.width
+        canvasMap.height = imageMap.height
+        contextMap = canvasMap.getContext('2d')
+        contextMap.drawImage(imageMap, 0, 0)
+        dataMap = contextMap.getImageData(0, 0, canvasMap.width, canvasMap.height)
 
-		// load earthcloudmaptrans
-		var imageTrans	= new Image();
-		imageTrans.addEventListener("load", function(){
-			// create dataTrans ImageData for earthcloudmaptrans
-			var canvasTrans		= document.createElement('canvas')
-			canvasTrans.width	= imageTrans.width
-			canvasTrans.height	= imageTrans.height
-			var contextTrans	= canvasTrans.getContext('2d')
-			contextTrans.drawImage(imageTrans, 0, 0)
-			var dataTrans		= contextTrans.getImageData(0, 0, canvasTrans.width, canvasTrans.height)
-			// merge dataMap + dataTrans into dataResult
-			var dataResult		= contextMap.createImageData(canvasMap.width, canvasMap.height)
-			for(var y = 0, offset = 0; y < imageMap.height; y++){
-				for(var x = 0; x < imageMap.width; x++, offset += 4){
-					dataResult.data[offset+0]	= dataMap.data[offset+0]
-					dataResult.data[offset+1]	= dataMap.data[offset+1]
-					dataResult.data[offset+2]	= dataMap.data[offset+2]
-					dataResult.data[offset+3]	= 255 - dataTrans.data[offset+0]
-				}
-			}
-			// update texture with result
-			contextResult.putImageData(dataResult,0,0)
-			material.map.needsUpdate = true;
-		})
-		imageTrans.src	= THREEx.Planets.baseURL+'images/earthcloudmaptrans.jpg';
-	}, false);
-	imageMap.src	= THREEx.Planets.baseURL+'images/earthcloudmap.jpg';
+        // load earthcloudmaptrans
+        imageTrans = new Image();
+        imageTrans.addEventListener("load", function() {
+            // create dataTrans ImageData for earthcloudmaptrans
+            var canvasTrans, contextTrans, dataTrans, dataResult;
+            canvasTrans = document.createElement('canvas')
+            canvasTrans.width = imageTrans.width
+            canvasTrans.height = imageTrans.height
+            contextTrans = canvasTrans.getContext('2d')
+            contextTrans.drawImage(imageTrans, 0, 0)
+            dataTrans = contextTrans.getImageData(0, 0, canvasTrans.width, canvasTrans.height)
+                // merge dataMap + dataTrans into dataResult
+            dataResult = contextMap.createImageData(canvasMap.width, canvasMap.height)
+            for (var y = 0, offset = 0, height = imageMap.height; y < height; y++) {
+                for (var x = 0, width = imageMap.width; x < width; x++, offset += 4) {
+                    dataResult.data[offset + 0] = dataMap.data[offset + 0]
+                    dataResult.data[offset + 1] = dataMap.data[offset + 1]
+                    dataResult.data[offset + 2] = dataMap.data[offset + 2]
+                    dataResult.data[offset + 3] = 255 - dataTrans.data[offset + 0]
+                }
+            }
+            // update texture with result
+            contextResult.putImageData(dataResult, 0, 0)
+            material.map.needsUpdate = true;
+        })
+        imageTrans.src = THREEx.Planets.baseURL + 'images/earthcloudmaptrans.jpg';
+    }, false);
+    imageMap.src = THREEx.Planets.baseURL + 'images/earthcloudmap.jpg';
 
-	var geometry	= new THREE.SphereGeometry(1.01, 32, 32)
-	var material	= new THREE.MeshPhongMaterial({
-		map		: new THREE.Texture(canvasResult),
-		side		: THREE.DoubleSide,
-		transparent	: true,
-		opacity		: 0.8,
-	})
-	var mesh	= new THREE.Mesh(geometry, material)
-	return mesh
+    geometry = new THREE.SphereGeometry(1.01, 32, 32)
+    material = new THREE.MeshPhongMaterial({
+        map: new THREE.Texture(canvasResult),
+        side: THREE.DoubleSide,
+        transparent: true,
+        opacity: 0.8,
+    })
+    mesh = new THREE.Mesh(geometry, material)
+    return mesh
 }
 
 
 
 
-THREEx.Planets.createStarfield	= function(){
-	var texture	= THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/galaxy_starfield.png')
-	var material	= new THREE.MeshBasicMaterial({
-		map	: texture,
-		side	: THREE.BackSide
-	})
-	var geometry	= new THREE.SphereGeometry(100, 32, 32)
-	var mesh	= new THREE.Mesh(geometry, material)
-	return mesh
+THREEx.Planets.createStarfield = function() {
+    var texture, material, geometry, mesh;
+    texture = THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL + 'images/galaxy_starfield.png')
+    material = new THREE.MeshBasicMaterial({
+        map: texture,
+        side: THREE.BackSide
+    })
+    geometry = new THREE.SphereGeometry(100, 32, 32)
+    mesh = new THREE.Mesh(geometry, material)
+    return mesh
 }
